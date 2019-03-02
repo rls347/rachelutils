d5fa4ef12ca5791ab2d9d626edb6e837ea035e74
Rachel Storer <rls347@gmail.com>
2019-03-01 18:14:16 -0800

    adding mass mean diameter calculation

--- micro.py
+++ micro.py
@@ -11,3 +11,37 @@ def calcgammadist(m,n,coef,ex,nu):
     func = (1/gamma(nu))*((dvals/dn)**ex)*(1/dn)*np.exp(-1*dvals/dn)
     return dvals, func
 
+def calcd(fil,var):
+    nnames = {  'pristine':'pris_concen_kg',
+                'cloud':'cloud_concen_mg',
+                'aggregates':'agg_concen_kg',
+                'snow':'snow_concen_kg',
+                'drizzle':'drizzle_concen_mg',
+                'hail':'hail_concen_kg',
+                'rain':'rain_concen_kg',
+                'graupel':'graup_concen_kg'
+            }
+    cfmas = { 'pristine': 110.8,
+              'cloud': 524.,
+              'aggregates': 0.496,
+              'snow': 0.002739,
+              'drizzle': 524.,
+              'hail': 471.,
+              'rain': 524.,
+              'graupel': 157.   }
+
+    pwmas = { 'cloud': 3.,
+              'drizzle': 3.,
+              'rain': 3.,
+              'pristine': 2.91,
+              'snow': 1.74,
+              'aggregates': 2.4,
+              'graupel': 3.,
+              'hail': 3.  }
+    mass = getvar(fil,var)
+    num = getvar(fil,nnames[var])
+    coef = cfmas[var]
+    ex = pwmas[var]
+
+    d=(mass/(num*coef))**(1./ex)
+    return d
